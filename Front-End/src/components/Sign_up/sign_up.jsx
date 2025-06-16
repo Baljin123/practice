7476aba5ba74467897e0ffc488ce35aeea9ded0d
Baljin123 <bss191735@gmail.com>
2025-06-16 23:26:40 +0530

    removed log

--- Front-End/src/components/Sign_up/sign_up.jsx
+++ Front-End/src/components/Sign_up/sign_up.jsx
@@ -28,7 +28,7 @@ const Sign_up = () => {
       event.preventDefault();
       const files = Array.from(event.dataTransfer.files);
       const data = { docuname: docuname, file: files[0] };
-      console.log("klsfhlkds");
+     
       
       setSelectedFiles([...selectedFiles, data]);
     };
